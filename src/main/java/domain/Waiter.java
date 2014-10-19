28de3d278abff84af8b406c673f6aa07c7590773
Ilya Lyamkin <xaker4544@gmail.com>
2014-10-19 04:27:14 +0400

    Added classes

--- src/main/java/domain/Waiter.java
+++ src/main/java/domain/Waiter.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
  * Created by Администратор on 18.10.2014.
  */
 @Entity
+@Table(name = "waiters")
 public class Waiter {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,6 +15,35 @@ public class Waiter {
     private String name;
     @Column
     private Double balance;
+    @Column
+    private Integer rating;
+    @Column
+    private String organization;
+
+    public Waiter() {
+    }
+
+    public Waiter(String name, String organization, Integer rating) {
+        this.name = name;
+        this.organization = organization;
+        this.rating = rating;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
 
     public Long getId() {
         return id;
--- src/main/java/servlet/GetUserProfileServlet.java
+++ src/main/java/servlet/GetUserProfileServlet.java
@@ -1,5 +1,7 @@
 package servlet;
 
+import domain.User;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.hibernate.Session;
@@ -20,9 +22,9 @@ public class GetUserProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Long id = Long.valueOf(req.getParameter("id"));
         Session session = HibernateUtil.getSessionAnnotationFactory().getCurrentSession();
+        session.beginTransaction();
         User user = (User) session.get(User.class, id);
-        HibernateUtil.getSessionAnnotationFactory().close();
-
+        session.getTransaction().commit();
         resp.setContentType("application/json");
         Gson gson = new GsonBuilder().create();
         PrintWriter out = resp.getWriter();
